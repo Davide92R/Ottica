3122090e9738a7f5c0da8cb2ad25da14bce887ad
Davide Roggero <davide.roggero12@gmail.com>
2022-10-21 09:16:13 +0200

    Initial commit

---
+++ resources/views/Ecografie.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1 class="text-center">Ecografie</h1>
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <header class="container-fluid img-bg vh-100">
+        <div class="pt-5">
+            <h1 class="text-center display-1">Studio M & D</h1>
+        </div>
+    </header>
+    <section class="container">
+        
+    </section>
+</x-layout>
